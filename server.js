0f519f642156a797a163155af86e076a9ae06b28
Dev-Rafael_Vieira <rafael_egqsociesc@hotmail.com>
2019-08-30 08:00:49 -0300

    adição da biblioteca 'cors'

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express'); // importa a biblioteca express
 const mongoose = require('mongoose'); // importa a biblioteca mongoose
 const requireDir = require('require-dir'); // importa a biblioteca require-dir
+const cors = require('cors'); // importa a biblioteca cors responsavel por liberar o acesso da aplicação a IPs externos
+
 
 
 //inicia o app
 const app = express(); // atribui a biblioteca a uma contante para manipular
 
 app.use(express.json()); //permite o envio de dados para aplicação no formato de JSON
+
+app.use(cors()); // faz a atribuição da biblioteca 'cors'
 
 // inicia o DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", {useNewUrlParser: true}); // 'mongodb://user@passworld'
